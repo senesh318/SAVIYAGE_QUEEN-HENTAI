5938f3a4a63a01e4633ff7258d358e92950cda66
senesh318 <131966282+senesh318@users.noreply.github.com>
2023-06-27 17:40:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94 72 601 3156', 'Saviya Official²⁰²³', true],
+  ['94 72 601 3156', 'Saviya Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
